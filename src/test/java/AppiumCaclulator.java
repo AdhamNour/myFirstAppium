78c22b27c76956f8c7e35281d9d7f3f63e5260c7
Adham Nour El-Waffaa <adhamnourelwaffaa@gmail.com>
2022-05-09 20:34:01 +0200

    finished video 8

--- src/test/java/AppiumCaclulator.java
+++ src/test/java/AppiumCaclulator.java
@@ -1,5 +1,6 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -20,13 +21,25 @@ public class AppiumCaclulator {
 
     public  static  void  openCalc() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("deviceName", "sdk_gphone64_x86_64");
+        capabilities.setCapability("deviceName", "Android SDK built for x86");
         capabilities.setCapability("udid","emulator-5554");
-        capabilities.setCapability("appPackage", "com.google.android.calculator");
-        capabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+        capabilities.setCapability("appPackage", "com.example.todo");
+        capabilities.setCapability("appActivity", "com.example.todo.MainActivity");
 
-        URL url = new URL("http://127.0.0.1:5723/wd/hub");
+        URL url = new URL("http://127.0.0.1:4725/wd/hub");
         driver = new AndroidDriver(url,capabilities);
         System.out.println("Application is Started ...");
+
+        var increment = driver.findElement(By.xpath(
+                "//android.widget.Button[@content-desc=\"Increment\"]"));
+        for (int i = 0; i < 10; i++) {
+            increment.click();
+        }
+        try {
+            var result = driver.findElement(By.xpath("//android.view.View[@content-desc=\"10\"]"));
+
+        } catch (Exception e) {
+            System.out.println("Failed");
+        }
     }
 }
